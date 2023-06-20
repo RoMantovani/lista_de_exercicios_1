86a2df0a9736a3c287201a5c738970cfe9996d37
RoMantovani <lancastermantovani@gmail.com>
2023-06-20 14:18:13 -0300

    Exercicio 6 finalizado

---
+++ exercicio_6.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, soma;
+
+    printf("Digite o primeiro numero: \n");
+    scanf("%d", &num1);
+
+    printf("Digite o segundo numero: \n");
+    scanf("%d", &num2);
+
+    soma = num1 + num2;
+
+    printf("A soma dos numeros eh: %d", soma);
+
+    return 0;
+}
