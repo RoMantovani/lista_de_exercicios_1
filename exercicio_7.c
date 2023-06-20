e002525e93ec9a8b654eca4c251e264ce813d983
RoMantovani <lancastermantovani@gmail.com>
2023-06-20 14:18:53 -0300

    Exercicio 7 finalizado

---
+++ exercicio_7.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    float num;
+
+    printf("Digite um numero real: \n");
+
+    scanf("%f", &num);
+
+    printf("A terca parte do numero  eh %.2f", num / 3);
+
+    return 0;
+}
