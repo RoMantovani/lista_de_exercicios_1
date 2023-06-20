e93b86cb024b9ce72aebed954498d553e7ebe6e8
RoMantovani <lancastermantovani@gmail.com>
2023-06-20 14:16:28 -0300

    Exercicio 5 finalizado

---
+++ exercicio_5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Digite um numero: \n");
+    scanf("%d", &num);
+
+    printf("O antecessor de %d eh %d\n", num, num - 1);
+    printf("O sucessor de %d eh %d\n", num, num + 1);
+
+    return 0;
+}
